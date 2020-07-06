ec241b3d3feb1f2c32e5d8c7d8d8ee7bf5e34bde
Ali Lopez <zero.exu@gmail.com>
2020-07-06 05:24:51 -0300

    test of router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import PropTypes from "prop-types";
 
 function WrapperAppZe(props) {
   return (
-    <div >
-     Hola {props.name}
-    </div>
+     <Router>
+      <div> Hola {props.name}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
@@ -14,3 +48,16 @@ WrapperAppZe.propTypes = {
 };
 
 export default WrapperAppZe;
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
